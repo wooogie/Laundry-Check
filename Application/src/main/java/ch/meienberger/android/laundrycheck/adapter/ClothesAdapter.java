e87a4e1fb0ff0355df5b8c08ae16679b92f98577
Silvan Meienberger <meiensil@students.zhaw.ch>
2017-12-27 00:33:39 +0100

    forgot

--- Application/src/main/java/ch/meienberger/android/laundrycheck/adapter/ClothesAdapter.java
+++ Application/src/main/java/ch/meienberger/android/laundrycheck/adapter/ClothesAdapter.java
@@ -51,183 +51,272 @@ import ch.meienberger.android.laundrycheck.R;
  * Provide views to RecyclerView with data from mDataSet.
  */
 public class ClothesAdapter extends RecyclerView.Adapter<ClothesAdapter.ViewHolder>  {
-    private static final String TAG = ClothesAdapter.class.getSimpleName();
-    private static ClothesDataSource mdataSource;
-    private ArrayList<Clothes> mDataSet;
-    private static Fragment mparentFragment;
-    private static ViewGroup mviewgroup;
-    private static Map<String,Bitmap> mbitmaps = new Hashtable<String,Bitmap>();
+        private static final String TAG = ClothesAdapter.class.getSimpleName();
+        private static ClothesDataSource mdataSource;
+        private ArrayList<Clothes> mDataSet;
+        private static Fragment mparentFragment;
+        private static ViewGroup mviewgroup;
+        private static Map<String,Bitmap> mbitmaps = new Hashtable<String,Bitmap>();
 
-    // BEGIN_INCLUDE
-    /**
-     * Provide a reference to the type of views that you are using (custom ViewHolder)
-     */
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        private final TextView name;
-        private final TextView id;
-        private final ImageView preview_image;
 
-        public ViewHolder(View v) {
-            super(v);
-            // Define click listener for the ViewHolder's View.
-            v.setOnClickListener(new View.OnClickListener() {
+        // BEGIN_INCLUDE
+        /**
+         * Provide a reference to the type of views that you are using (custom ViewHolder)
+         */
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            private final TextView name;
+            private final TextView id;
+            private final ImageView preview_image;
+
+
+
+            public ViewHolder(View v) {
+                super(v);
+                // Define click listener for the ViewHolder's View.
+                v.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.d(TAG, "Clothes with Id: " + getId().getText() + " clicked.");
+
+                        // Create new fragment with the ClothesId as arg and a new transaction
+                        Fragment DetailFragment = new ClothesDetailViewFragment();
+                        Bundle args = new Bundle();
+                        args.putLong(ClothesDetailViewFragment.ARG_CLOTHESID, Long.parseLong(getId().getText().toString()));
+                        DetailFragment.setArguments(args);
+
+                        android.support.v4.app.FragmentTransaction transaction = mparentFragment.getFragmentManager().beginTransaction();
+
+                        // Replace whatever is in the fragment_container view with this fragment,
+                        // and add the transaction to the back stack if needed
+                        transaction.replace(R.id.content_fragment, DetailFragment);
+                        transaction.addToBackStack(null);
+
+                        // Commit the transaction
+                        transaction.commit();
+
+                    }
+                });
+
+
+
+                //link view holder to xml layout
+                name = (TextView) v.findViewById(R.id.clothesrow_name_textview);
+                id = (TextView) v.findViewById(R.id.clothesrow_id_textview);
+                preview_image = (ImageView) v.findViewById(R.id.clothesrowitem_preview_imageView);
+            }
+
+            public TextView getName() {
+                return name;
+            }
+            public TextView getId() {
+                return id;
+            }
+            public ImageView getPreview_image() {return preview_image; }
+
+        }
+
+        /**
+         * Initialize the dataset of the Adapter.
+         *
+         * @param dataSet String[] containing the data to populate views to be used by RecyclerView.
+         */
+        public ClothesAdapter(ArrayList<Clothes> dataSet, ClothesDataSource dataSource, Fragment parentFragment) {
+            mDataSet = dataSet;
+            mdataSource = dataSource;
+            mparentFragment = parentFragment;
+        }
+
+        // BEGIN_INCLUDE(recyclerViewOnCreateViewHolder)
+        // Create new views (invoked by the layout manager)
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            // Create a new view.
+            View v = LayoutInflater.from(viewGroup.getContext())
+                    .inflate(R.layout.clothes_row_item, viewGroup, false);
+
+            mviewgroup = viewGroup;
+
+            return new ViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+            Log.d(TAG, "Clothes " + position + " set.");
+
+            // Get element from your dataset at this position and replace the contents of the view
+            // with that element
+            viewHolder.getName().setText(mDataSet.get(position).getName());
+
+            String tmp_string = Long.toString(mDataSet.get(position).getId());
+            viewHolder.getId().setText(tmp_string);
+
+            Bitmap curBitmap = mbitmaps.get(mDataSet.get(position).getPicture());
+            if(curBitmap == null) {
+                //Create for all preview pictures a own Task for getting the right resolution
+                //Set Picturepreview by a new thread
+                LoadPreviewPictureTask loadpreviewpicture = new LoadPreviewPictureTask();
+                loadpreviewpicture.execute(
+                        String.valueOf(position),
+                        String.valueOf(mparentFragment.getResources().getDimensionPixelSize(R.dimen.image_preview_with)),
+                        String.valueOf(mparentFragment.getResources().getDimensionPixelSize(R.dimen.list_item_height)),
+                        mDataSet.get(position).getPicture()
+                );
+            }else{
+                //Set bitmap to the image viewholder
+                viewHolder.getPreview_image().setImageBitmap(curBitmap);
+            }
+
+        }
+        // END_INCLUDE(recyclerViewOnBindViewHolder)
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataSet.size();
+        }
+
+        public void addItem() {
+
+            //generate Name
+            Calendar c = Calendar.getInstance();
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+            String formattedDate = df.format(c.getTime());
+
+
+            //generate new clothes
+            mdataSource.open();
+            mdataSource.createClothes(formattedDate);
+            mDataSet = (ArrayList<Clothes>)mdataSource.getAllClothes();
+            mdataSource.close();
+
+            notifyItemInserted(0);
+            notifyItemChanged(0);
+        }
+
+
+        public void removeItemAt(int position) {
+            final Clothes toDeleteClothes = mDataSet.get(position);
+            final int cur_Position = position;
+
+
+
+            //double check if the click was not a misstake
+            AlertDialog.Builder builder = new AlertDialog.Builder(mparentFragment.getContext());
+            builder.setMessage(R.string.confirm_deleteClothes_message)
+                    .setTitle(R.string.confirm_delete_title);
+            builder.setCancelable(true);
+            builder.setNegativeButton(R.string.yes, new DialogInterface.OnClickListener() {
                 @Override
-                public void onClick(View v) {
-                    Log.d(TAG, "Clothes with Id: " + getId().getText() + " clicked.");
+                public void onClick(DialogInterface dialog, int which) {
 
-                    // Create new fragment with the ClothesId as arg and a new transaction
-                    Fragment DetailFragment = new ClothesDetailViewFragment();
-                    Bundle args = new Bundle();
-                    args.putLong(ClothesDetailViewFragment.ARG_CLOTHESID, Long.parseLong(getId().getText().toString()));
-                    DetailFragment.setArguments(args);
+                    //delete picture
+                    File oldPhotoFile = new File(toDeleteClothes.getPicture());
+                    oldPhotoFile.delete();
 
-                    android.support.v4.app.FragmentTransaction transaction = mparentFragment.getFragmentManager().beginTransaction();
+                    //delete from DB
+                    mdataSource.open();
+                    mdataSource.deleteClothes(toDeleteClothes);
+                    mdataSource.close();
 
-                    // Replace whatever is in the fragment_container view with this fragment,
-                    // and add the transaction to the back stack if needed
-                    transaction.replace(R.id.content_fragment, DetailFragment);
-                    transaction.addToBackStack(null);
+                    //remove from Dataset
+                    mDataSet.remove(cur_Position);
+                    notifyItemRemoved(cur_Position);
 
-                    // Commit the transaction
-                    transaction.commit();
-
-                }
+                               }
             });
 
-
-
-            //link view holder to xml layout
-            name = (TextView) v.findViewById(R.id.clothesrow_name_textview);
-            id = (TextView) v.findViewById(R.id.clothesrow_id_textview);
-            preview_image = (ImageView) v.findViewById(R.id.clothesrowitem_preview_imageView);
+            builder.setPositiveButton(R.string.no, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    notifyItemChanged(cur_Position);
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
         }
 
-        public TextView getName() {
-            return name;
-        }
-        public TextView getId() {
-            return id;
-        }
-        public ImageView getPreview_image() {return preview_image; }
-
-    }
-
-    /**
-     * Initialize the dataset of the Adapter.
-     *
-     * @param dataSet String[] containing the data to populate views to be used by RecyclerView.
-     */
-    public ClothesAdapter(ArrayList<Clothes> dataSet, ClothesDataSource dataSource, Fragment parentFragment) {
-        mDataSet = dataSet;
-        mdataSource = dataSource;
-        mparentFragment = parentFragment;
-    }
-
-    // BEGIN_INCLUDE(recyclerViewOnCreateViewHolder)
-    // Create new views (invoked by the layout manager)
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        // Create a new view.
-        View v = LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.clothes_row_item, viewGroup, false);
-
-        mviewgroup = viewGroup;
-
-        return new ViewHolder(v);
-    }
-
-    @Override
-    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
-        Log.d(TAG, "Clothes " + position + " set.");
-
-        // Get element from your dataset at this position and replace the contents of the view
-        // with that element
-        viewHolder.getName().setText(mDataSet.get(position).getName());
-
-        String tmp_string = Long.toString(mDataSet.get(position).getId());
-        viewHolder.getId().setText(tmp_string);
-
-        Bitmap curBitmap = mbitmaps.get(mDataSet.get(position).getPicture());
-        if(curBitmap != null){
-            viewHolder.getPreview_image().setImageBitmap(curBitmap);
-        }
-
-    }
-    // END_INCLUDE(recyclerViewOnBindViewHolder)
-
-    // Return the size of your dataset (invoked by the layout manager)
-    @Override
-    public int getItemCount() {
-        return mDataSet.size();
-    }
-
-    public void addItem() {
-
-        //generate Name
-        Calendar c = Calendar.getInstance();
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
-        String formattedDate = df.format(c.getTime());
-
-
-        //generate new clothes
-        mdataSource.open();
-        mdataSource.createClothes(formattedDate);
-        mDataSet = (ArrayList<Clothes>)mdataSource.getAllClothes();
-        mdataSource.close();
-
-        notifyItemInserted(0);
-        notifyItemChanged(0);
-    }
-
-
-    public void removeItemAt(int position) {
-        final Clothes toDeleteClothes = mDataSet.get(position);
-        final int cur_Position = position;
-
-
-
-        //double check if the click was not a misstake
-        AlertDialog.Builder builder = new AlertDialog.Builder(mparentFragment.getContext());
-        builder.setMessage(R.string.confirm_deleteClothes_message)
-                .setTitle(R.string.confirm_delete_title);
-        builder.setCancelable(true);
-        builder.setNegativeButton(R.string.yes, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-                //delete picture
-                File oldPhotoFile = new File(toDeleteClothes.getPicture());
-                oldPhotoFile.delete();
-
-                //delete from DB
-                mdataSource.open();
-                mdataSource.deleteClothes(toDeleteClothes);
-                mdataSource.close();
-
-                //remove from Dataset
-                mDataSet.remove(cur_Position);
-                notifyItemRemoved(cur_Position);
-
-                           }
-        });
-
-        builder.setPositiveButton(R.string.no, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                notifyItemChanged(cur_Position);
+        public void updatePreviewpicture(int position, Bitmap bitmap) {
+            String curPicturepath = mDataSet.get(position).getPicture();
+            if (!curPicturepath.isEmpty()){
+                mbitmaps.put(curPicturepath,bitmap);
+                notifyItemChanged(position);
             }
-        });
-        AlertDialog dialog = builder.create();
-        dialog.show();
-    }
-
-    public void updatePreviewpicture(int position, Bitmap bitmap) {
-        String curPicturepath = mDataSet.get(position).getPicture();
-        if (!curPicturepath.isEmpty()){
-            mbitmaps.put(curPicturepath,bitmap);
-            notifyItemChanged(position);
         }
-    }
 
 
-    }
+
+        /**
+         * This task loads the picture in the right resolution into the Viewholder.
+         * It is going to run in the background because it need much time to do the downscaling and intrrupt else the UI.
+         * params([0]=position,[1]=imageWith,[2]=imageHeight,[3]=Picturepath,
+         * */
+        public class LoadPreviewPictureTask extends AsyncTask<String, Integer, LoadPreviewPictureTask.TaskResult> {
+
+            private final String LOG_TAG = LoadPreviewPictureTask.class.getSimpleName();
+
+            public final class TaskResult {
+                private Bitmap mbitmap;
+                private int mposition;
+
+                TaskResult(Bitmap bitmap, int position){
+                    mbitmap = bitmap;
+                    mposition = position;
+                }
+
+                public Bitmap getBitmap() {
+                    return mbitmap;
+                }
+
+                public int getPosition() {
+                    return mposition;
+                }
+            }
+
+
+            protected ClothesAdapter.LoadPreviewPictureTask.TaskResult doInBackground(String... params) {
+
+                // Get Parameter of task
+                int position = Integer.parseInt(params[0]);
+                int targetW = Integer.parseInt(params[1]);
+                int targetH = Integer.parseInt(params[2]);
+                String picturepath = String.valueOf(params[3]);
+
+
+                // Get the dimensions of the bitmap
+                BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+                bmOptions.inJustDecodeBounds = true;
+                BitmapFactory.decodeFile(picturepath.replace("file:", ""), bmOptions);
+                int photoW = bmOptions.outWidth;
+                int photoH = bmOptions.outHeight;
+
+                // Determine how much to scale down the image
+                int scaleFactor = 1;
+                if (photoH > targetH || photoW > targetW) {
+                    scaleFactor = photoW > photoH
+                            ? photoH / targetH
+                            : photoW / targetW;
+                }
+
+
+                // Decode the image file into a Bitmap sized to fill the View
+                bmOptions.inJustDecodeBounds = false;
+                bmOptions.inSampleSize = scaleFactor;
+
+                Bitmap bitmap = BitmapFactory.decodeFile(picturepath.replace("file:",""), bmOptions);
+
+
+                return new TaskResult(bitmap,position);
+            }
+
+
+            protected void onPostExecute(TaskResult result) {
+                Log.d(TAG, "hintergroundtask fertig mit " + result.getPosition());
+
+                //set bitmap
+                updatePreviewpicture(result.getPosition(),result.mbitmap);
+            }
+        }
+
+
+}
+
